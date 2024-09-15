570b7f759cd9c9e6b3fd91c563d3e96439cba98f
Felipe Fernandes <ff8778732@gmail.com>
2024-09-15 15:36:06 -0300

    commit 1

---
+++ select.sql
@@ -0,0 +1,3 @@
+SELECT * FROM livros;
+
+insert into livros(titulo) values ("pequeno principe");
---
+++ tdd.sql
@@ -0,0 +1,7 @@
+create table livros(
+    id int auto_increment primary key,
+    titulo varchar(100) not null
+    
+);
+
+//Ctrl+Shift+P
